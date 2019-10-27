e5e27d748751a409784100bc4fa2879c34edc934
Ryan Baxley <ryan.baxley1@gmail.com>
2019-10-27 12:36:31 -0400

    Add all props

--- App.js
+++ App.js
@@ -1,17 +1,73 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Button } from 'react-native';
 import Plotly from 'react-native-plotly';
 
-export default function App() {
-  const data = [{
-    x: [1, 2, 3, 4, 5],
-    y: [1, 2, 3, 4, 8],
-  }];
-  const layout = { title: 'My cool chart!' };
+const upData = {
+  __id: 'up',
+  x: [1, 2, 3, 4, 5],
+  y: [1, 2, 3, 4, 8],
+  type: 'scatter'
+};
 
-  return (
-    <View style={{ flex: 1 }}>
-      <Plotly data={data} layout={layout} />
-    </View>
-  );
+const downData = {
+  __id: 'down',
+  x: [1, 2, 3, 4, 5],
+  y: [8, 4, 3, 2, 1],
+  type: 'scatter'
+};
+
+export default class App extends React.Component {
+  state = {
+    data: [upData],
+    layout: { title: 'Plotly.js running in React Native!' }
+  };
+
+  swapData = () => {
+    if (this.state.data[0].__id === 'up') {
+      this.setState({ data: [downData] });
+    } else {
+      this.setState({ data: [upData] });
+    }
+  };
+
+  update = (_, { data, layout, config }, plotly) => {
+    plotly.react(data, layout, config);
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.buttonRow}>
+          <Button onPress={this.swapData} title="Swap Data" />
+        </View>
+        <View style={styles.chartRow}>
+          <Plotly
+            data={this.state.data}
+            layout={this.state.layout}
+            update={this.update}
+            onLoad={() => console.log('loaded')}
+            debug
+          />
+        </View>
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  buttonRow: {
+    flexDirection: 'row'
+  },
+  chartRow: {
+    flex: 1,
+    width: '100%'
+  },
+  container: {
+    paddingTop: 30,
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
